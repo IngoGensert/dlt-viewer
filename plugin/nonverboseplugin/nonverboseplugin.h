5933e6553b0955bd1f4482cafaf5706506fbf6c0
IngoGensert <Ingo@gensert4.de>
2026-09-15 16:22:05 +0200

    Update Plugin Name with offset informaton

--- plugin/nonverboseplugin/nonverboseplugin.h
+++ plugin/nonverboseplugin/nonverboseplugin.h
@@ -31,7 +31,7 @@
 #endif
 
 #define NON_VERBOSE_PLUGIN_VERSION "1.0.0"
-#define NON_VERBOSE_PLUGIN_NAME "Non Verbose Mode Plugin"
+#define NON_VERBOSE_PLUGIN_NAME "Non Verbose Mode Plugin (offset 9)"
 
 class DltFibexKey
 {
