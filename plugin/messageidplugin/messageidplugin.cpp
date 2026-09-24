a9e39c641178d96f984e12508fb4d7bdcac464d0
IngoGensert <Ingo@gensert4.de>
2026-09-24 08:50:49 +0200

    Refactor isMsg function to streamline logic
    Simplified the isMsg function by removing unnecessary checks for message mode and type.

--- plugin/messageidplugin/messageidplugin.cpp
+++ plugin/messageidplugin/messageidplugin.cpp
@@ -532,25 +532,9 @@ bool MessageIdPlugin::isMsg(QDltMsg &msg, int triggeredByUser)
 {
     Q_UNUSED(triggeredByUser)
 
-    if((msg.getMode() != QDltMsg::DltModeNonVerbose))
-    {
-        /* message is not a non-verbose message */
-        return false;
-    }
-    if((msg.getType() == QDltMsg::DltTypeControl))
-    {
-        /* message is a control message */
-        return false;
-    }
-
     QString idtext = QString("ID_%1").arg(msg.getMessageId());
 
-    if(!msg.getApid().isEmpty() && !msg.getCtid().isEmpty())
-        // search in full key, if msg already contains AppId and CtId
-        return framemapwithkey.contains(DltFibexKey(idtext,msg.getApid(),msg.getCtid()));
-    else
-        // search only for id
-        return framemap.contains(idtext);
+    return framemap.contains(idtext);
 }
 
 bool MessageIdPlugin::decodeMsg(QDltMsg &msg, int triggeredByUser)
